55b5ab9951d83a30686acb201152687df0904697
Tyler Sherknus <tylersherknus@gmail.com>
2026-05-31 14:45:37 -0400

    * initialize user info

---
+++ supabase/functions/initialize-user-info/index.ts
@@ -0,0 +1,57 @@
+import { createClient } from "@supabase/supabase-js";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "https://vite-tailwind-supabase-vercel-start.vercel.app",
+  "Access-Control-Allow-Headers": "authorization, apikey, content-type, x-client-info",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+};
+
+Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", {
+      headers: corsHeaders
+    });
+  }
+
+  try {
+    const { data } = await req.json();
+
+    console.log(data);
+
+    console.log(Deno.env.get("SUPABASE_URL"));
+
+    console.log(Deno.env.get("SUPABASE_ANON_KEY"));
+
+    const supabaseUrl = Deno.env.get("SUPABASE_URL");
+    const supabaseServiceKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
+
+    const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+    const { error } = await supabase
+        .from("user_info")
+        .insert({
+          id: data,
+        })
+
+    if (error) {
+      throw error;
+    }
+
+    return new Response(JSON.stringify({ response: "success" }), {
+      status: 200,
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "application/json"
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: "Internal server error" }), {
+      status: 500,
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "application/json"
+      },
+    });
+  }
+});
